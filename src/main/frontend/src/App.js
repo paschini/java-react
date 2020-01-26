a92c45e9e4e5961f34de12b33c7eedf03061d679
Camila Paschini <camila.paschini@gmail.com>
2020-01-26 14:52:23 +0100

    Moved the data fetching into a custom hook

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -3,23 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [message, setMessage] = useState('');
-  const [loading, setLoading] = useState(true);
+  const useMessage = () => {
+    const [message, setMessage] = useState('');
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const getMessage = async () => {
-      try {
-        setLoading(false);
-        const result = await fetch('api/hello');
-        return await result.text();
-      } catch (error) {
-        setLoading(true);
-        console.log(error);
-      }
-    };
+    useEffect(() => {
+      const getMessage = async () => {
+        try {
+          setLoading(false);
+          const result = await fetch('api/hello');
+          return await result.text();
+        } catch (error) {
+          setLoading(true);
+          console.log(error);
+        }
+      };
 
-    getMessage().then(msg => setMessage(msg));
-  }, []);
+      getMessage().then(msg => setMessage(msg));
+    }, []);
+
+    return [loading, message];
+  };
+
+  const [loading, message] = useMessage();
 
   return (
     <div className="App">
